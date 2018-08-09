856fc9972efb752b71d63937695a10e195f61751
Kimberley Johnson <kimjaydot@gmail.com>
2018-08-08 19:26:19 -0500

    forgot javascript iteration

--- jstest.js
+++ jstest.js
@@ -1,9 +1,9 @@
 var firstName = ["Smokey", "Waffles", "Simba"];
 var lastName = ["Johnson", "Yun", "Kevin"];
 var combinedNames = [];
-for(var i = 0 ; firstName.length; i++){
-    for(var x = 0; lastName.length; i++){
-        combinedNames.push(firstNames[i] + lastNames[x]);
+for (var i = 0 ; i < firstName.length; i++){
+    for (var x = 0; i < lastName.length; i++){
+        combinedNames.push(firstName[i] + lastName[x]);
         console.log(combinedNames);
         alert(combinedNames);
     }
