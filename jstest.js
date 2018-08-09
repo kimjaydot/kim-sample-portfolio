1dc0600faf9906271203f736dd3304563d1be224
Kimberley Johnson <kimjaydot@gmail.com>
2018-08-08 19:09:25 -0500

    Add test js file

---
+++ jstest.js
@@ -0,0 +1,8 @@
+var firstName = ["Smokey", "Waffles", "Simba"];
+var lastName = ["Johnson", "Yun", "Kevin"];
+var combinedNames = [];
+for(var i = 0 ; firstName.length; i++){
+    for(var x = 0; lastName.length; i++){
+        combinedNames.push([i] + [x]);
+    }
+}
