1febe6c904ca34a4329560bdae8a980348b6305b
Kimberley Johnson <kimjaydot@gmail.com>
2018-08-08 19:12:02 -0500

    Updating Files

--- jstest.js
+++ jstest.js
@@ -4,5 +4,6 @@ var combinedNames = [];
 for(var i = 0 ; firstName.length; i++){
     for(var x = 0; lastName.length; i++){
         combinedNames.push([i] + [x]);
+        console.log(combinedNames);
     }
 }
